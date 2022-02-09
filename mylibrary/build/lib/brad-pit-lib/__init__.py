e3612a423cc3f30fd20712370307a2c79d8ab010
Magda <magdalenar@onet.com.pl>
2022-02-09 15:14:33 +0100

    pip installable package

--- mylibrary/build/lib/brad-pit-lib/__init__.py
+++
@@ -1,8 +0,0 @@
-from .graph import create_table, create_hist, visualize
-from .income import prepare_file, compare_income, double_names, drop_double_names
-from .params import param, full_param
-from .people import ppl_income_w, ppl_income_p, ppl_income_g
-
-#importowanie, żeby potem przy użyciu można było robić "from bradpitlib import full_param"
-#zamiast "from bradpitlib.params import full_param"
-#(chyba)
