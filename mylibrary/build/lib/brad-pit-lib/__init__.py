01f2a42aacdcde1bdca2b84e2eeef681ec175eb3
Magda <magdalenar@onet.com.pl>
2022-02-09 11:35:11 +0100

    built package

--- mylibrary/build/lib/brad-pit-lib/__init__.py
+++ mylibrary/build/lib/brad-pit-lib/__init__.py
@@ -3,6 +3,6 @@ from .income import prepare_file, compare_income, double_names, drop_double_name
 from .params import param, full_param
 from .people import ppl_income_w, ppl_income_p, ppl_income_g
 
-#importowanie, żeby potem przy użyciu można było robić "from brad-pit-lib import full_param"
-#zamiast "from brad-pit-lib.params import full_param"
+#importowanie, żeby potem przy użyciu można było robić "from bradpitlib import full_param"
+#zamiast "from bradpitlib.params import full_param"
 #(chyba)
