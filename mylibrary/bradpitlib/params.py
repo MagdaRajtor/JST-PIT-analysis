351110d9ceffb654c5e30e19cb5d0330753cd396
MagdaRajtor <93554387+MagdaRajtor@users.noreply.github.com>
2023-11-07 16:55:54 +0100

    translate comments

--- mylibrary/bradpitlib/params.py
+++ mylibrary/bradpitlib/params.py
@@ -1,18 +1,19 @@
 import pandas as pd
 
 def param(type, w_file, p_file, w_name, p_name=None, g_file=None):
-    """type='var' policzy wariancję, a 'mean' średnią; funkcja dla pojedycznego województwa lub powiatu + województwa"""
+    """for a single województwo or powiat + województwo;
+    type='var' calculates the variance, type='mean' - the mean"""
     row = {}
     if p_name is None:
-        # trzymamy dane jednostki nadrzędnej
+        # keep the data for the parent unit
         upper = w_file.loc[w_file["nazwa"] == w_name]
-        code = upper.iloc[0, 0][:2] # 2 pierwsze cyfry mają znaczenie
+        code = upper.iloc[0, 0][:2] # 2 first digits of the codes are significant
         pom = p_file.loc[p_file["województwo"] == w_name]
         df = pom[pom["kod"].str.match(code)]
     else:
-        # tutaj i powiat i województwo, bo nazwy powiatów się powtarzają (np. tomaszowski)
+        # powiat and województwo are kept together, since powiat names themselves do repeat (e.g. tomaszowski)
         upper = p_file.loc[(p_file["nazwa"] == p_name) & (p_file["województwo"] == w_name)]
-        code = upper.iloc[0, 0][:4] # 4 pierwsze cyfry mają znaczenie
+        code = upper.iloc[0, 0][:4] # 4 first digits of the codes are significant
         pom = g_file.loc[g_file["powiat"] == p_name]
         df = pom[pom["kod"].str.match(code)]
     if type == "var":
@@ -20,14 +21,14 @@ def param(type, w_file, p_file, w_name, p_name=None, g_file=None):
         row = {"kod": code, "nazwa": upper.iloc[0,3], "v2019": result[0], "v2020": result[1]}
     elif type == "mean":
         result = df[["2019", "2020"]].mean()
-        # od razu z dochodami dla całej JST dla obu lat
+        # combine with the incomes for JST for both years
         row = {"kod": code, "nazwa": upper.iloc[0,3], "m2019": result[0], "m2020": result[1], \
                "2019": upper.iloc[0,1], "2020": upper.iloc[0,2]}
     return row
 
 
 def full_param(type, w_file, p_file, g_file=None, powiat=False):
-    """dla wszystkich województw lub powiatów"""
+    """for all województwa or powiaty"""
     result = pd.DataFrame()
     names = w_file["nazwa"].tolist()
     ex_names = [None] * w_file.shape[0]
